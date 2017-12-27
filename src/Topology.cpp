891557d28e022b457f7d7a9e0bd350d83896cae7
Peter Gazdik <xgazdi03@stud.fit.vutbr.cz>
2017-12-27 14:34:53 +0100

    Delete useless source files

--- src/Topology.cpp
+++
@@ -1,8 +0,0 @@
-/**
- * @author      Peter Gazdík <xgazdi03(at)stud.fit.vutbr.cz>
- *              Michal Klčo <xklcom00(at)stud.fit.vutbr.cz>
- * @date        02/12/17
- * @copyright   The MIT License (MIT)
- */
-
-#include "Topology.h"
