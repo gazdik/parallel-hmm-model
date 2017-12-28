27100571f9385ed879b85467ca0c4aa48027d233
Peter Gazdik <xgazdi03@stud.fit.vutbr.cz>
2017-12-28 10:39:26 +0100

    Add the script that archives the src files and the documentation

---
+++ pack.sh
@@ -0,0 +1,16 @@
+#/bin/bash
+
+if [ -z "$1" ]
+then
+	echo "Run the script with your login as the first parameter."
+	exit 2
+fi
+
+cd doc
+make
+cd ..
+
+mkdir "$1"
+cp -t "$1" -r src/ CMakeLists.txt doc/gmu.pdf
+zip -r "$1".zip "$1"
+rm -r "$1"
